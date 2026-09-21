f123058108a394f2ab26cb858a5f55f728895385
Mariusz <mariuszmalankiewicz@gmail.com>
2026-09-21 20:37:33 +0200

    feat: add ATR calculate and tests

--- src/indicators/atr.py
+++ src/indicators/atr.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
 
+from indicators.validation import validate_is_series, validate_period
+
 
 def calculate_true_range(data: pd.DataFrame) -> pd.Series:
     if not isinstance(data, pd.DataFrame):
@@ -16,3 +18,25 @@ def calculate_true_range(data: pd.DataFrame) -> pd.Series:
         trs.loc[index] = tr_max
         previus_close = row["Close"]
     return trs
+
+
+def calculate_atr(tr: pd.Series, period: int) -> pd.Series:
+    validate_is_series(tr, "True range")
+    validate_period(period)
+    atr = pd.Series(dtype=float)
+    for index, value in enumerate(tr):
+        temp_atr = 0
+        count = 0
+        if index >= period - 1:
+            for index_inner, value_inner in enumerate(tr):
+                if index_inner <= index and index_inner >= index - period + 1:
+                    temp_atr += value_inner
+                    count += 1
+                    if count == period:
+                        temp_atr = temp_atr / period
+                        atr[tr.index[index]] = temp_atr
+                        count = 0
+                        temp_atr = 0
+        else:
+            atr[tr.index[index]] = np.nan
+    return atr
--- tests/indicators/test_atr.py
+++ tests/indicators/test_atr.py
@@ -1,7 +1,8 @@
+import numpy as np
 import pandas as pd
 import pytest
 
-from indicators.atr import calculate_true_range
+from indicators.atr import calculate_atr, calculate_true_range
 
 
 def test_calculate_true_range_type_error_invalid_data():
@@ -37,3 +38,45 @@ def test_calculate_true_range_return_correct_values():
         index=["2026-09-01", "2026-09-02", "2026-09-03", "2026-09-04", "2026-09-05"],
     )
     assert pd.Series.equals(result, expected)
+
+
+def test_calculate_atr_type_error_invalid_tr():
+    with pytest.raises(TypeError):
+        tr = []
+        period = 2
+        calculate_atr(tr, period)
+
+
+def test_calculate_atr_type_error_invalid_period():
+    with pytest.raises(TypeError):
+        tr = pd.Series([10, 20, 30])
+        period = "2"
+        calculate_atr(tr, period)
+
+
+def test_calculate_atr_type_value_invalid_period():
+    with pytest.raises(ValueError):
+        tr = pd.Series([10, 20, 30])
+        period = 0
+        calculate_atr(tr, period)
+
+
+def test_calculate_atr_returns_correct_structure():
+    tr = pd.Series([10, 20, 30])
+    period = 2
+    calculate_atr(tr, period)
+    assert isinstance(calculate_atr(tr, period), pd.Series)
+
+
+def test_calculate_atr_returns_correct_values():
+    tr = pd.Series(
+        [5, 6, 6, 4, 4],
+        index=["2026-09-01", "2026-09-02", "2026-09-03", "2026-09-04", "2026-09-05"],
+    )
+    period = 3
+    result = calculate_atr(tr, period)
+    excepted = pd.Series(
+        [np.nan, np.nan, 5.666667, 5.333333, 4.666667],
+        index=["2026-09-01", "2026-09-02", "2026-09-03", "2026-09-04", "2026-09-05"],
+    )
+    assert pd.Series.equals(round(result, 2), round(excepted, 2))
