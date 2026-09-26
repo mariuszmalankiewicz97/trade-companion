4f6785bf0527605d8d68b7a57a3147d7ea226992
Mariusz <mariuszmalankiewicz@gmail.com>
2026-09-26 12:39:01 +0200

    refactor: update find and calculate zone for pivots low and high and tests

--- src/support_resistance.py
+++ src/support_resistance.py
@@ -3,43 +3,73 @@ import pandas as pd
 from indicators.validation import validate_is_series, validate_period
 
 
-def find_pivot_lows(low: pd.Series, period: int) -> pd.Series:
+def find_pivot_lows(low: pd.Series, period: int = 2) -> pd.Series:
     validate_is_series(low, "Low")
     validate_period(period)
-    pivots = pd.Series(dtype=float)
-    for index, value in enumerate(low):
-        count = 0
-        if index >= period and index < len(low) - period:
-            for inner_index, inner_value in enumerate(low):
-                if (
-                    inner_index >= index - period
-                    and inner_index <= index + period
-                    and inner_index != index
-                ):
-                    if value <= inner_value:
-                        count += 1
-                    if count == 2 * period:
-                        pivots.at[index] = value
-                        count = 0
-    return pivots
+    pivots = {}
+    length = len(low)
+    start = period
+    stop = length - period
+    for index in range(start, stop):
+        if (
+            low.iloc[index] < low.iloc[index - period + 1] < low.iloc[index - period]
+            and low.iloc[index]
+            < low.iloc[index + period - 1]
+            < low.iloc[index + period]
+        ):
+            pivots[low.index[index]] = low.iloc[index]
+    return pd.Series(pivots, dtype=float)
 
 
-def find_pivot_highs(high: pd.Series, period: int) -> pd.Series:
+def find_the_low_price_zones(low_pivots: pd.Series, atr: pd.Series) -> pd.DataFrame:
+    validate_is_series(low_pivots, "Pivots")
+    validate_is_series(atr, "ATR")
+    length = len(low_pivots)
+    start = 0
+    stop = length
+    groups = []
+    for index in range(start, stop):
+        date = low_pivots.index[index]
+        bottom_value = low_pivots.iloc[index]
+        atr_value = atr[date]
+        top_value = 0.3 * atr_value + bottom_value
+        groups.append(
+            {"date": date, "type": "low", "bottom": bottom_value, "top": top_value}
+        )
+    return pd.DataFrame(groups)
+
+
+def find_pivot_high(high: pd.Series, period: int = 2) -> pd.Series:
     validate_is_series(high, "High")
-    validate_period(period, "Period")
-    pivots = pd.Series()
-    for index, value in enumerate(high):
-        count = 0
-        if index >= period and index < len(high) - period:
-            for inner_index, inner_value in enumerate(high):
-                if (
-                    index != inner_index
-                    and inner_index >= index - period
-                    and inner_index <= index + period
-                ):
-                    if value > inner_value:
-                        count += 1
-                    if count == period * 2:
-                        pivots.at[index] = value
-                        count = 0
-    return pivots
+    validate_period(period)
+    pivots = {}
+    length = len(high)
+    start = period
+    stop = length - period
+    for index in range(start, stop):
+        if (
+            high.iloc[index] > high.iloc[index - period + 1] > high.iloc[index - period]
+            and high.iloc[index]
+            > high.iloc[index + period - 1]
+            > high.iloc[index + period]
+        ):
+            pivots[high.index[index]] = high.iloc[index]
+    return pd.Series(pivots, dtype=float)
+
+
+def find_the_high_price_zones(high_pivots: pd.Series, atr: pd.Series) -> pd.DataFrame:
+    validate_is_series(high_pivots, "Pivots")
+    validate_is_series(atr, "ATR")
+    length = len(high_pivots)
+    start = 0
+    stop = length
+    groups = []
+    for index in range(start, stop):
+        date = high_pivots.index[index]
+        top_value = high_pivots.iloc[index]
+        atr_value = atr[date]
+        bottom_value = top_value - 0.3 * atr_value
+        groups.append(
+            {"date": date, "type": "high", "top": top_value, "bottom": bottom_value}
+        )
+    return pd.DataFrame(groups)
--- tests/test_support_resistance.py
+++ tests/test_support_resistance.py
@@ -1,76 +1,203 @@
 import pandas as pd
 import pytest
 
-from support_resistance import find_pivot_highs, find_pivot_lows
+from support_resistance import (
+    find_pivot_high,
+    find_pivot_lows,
+    find_the_high_price_zones,
+    find_the_low_price_zones,
+)
+
+###
+# FIND PIVOTS LOWS
+###
 
 
-def test_find_pivot_lows_raises_type_error_for_invalid_low():
+def test_find_pivot_lows_type_error_invalid_low():
     with pytest.raises(TypeError):
-        low = [10, 12, 14]
+        low = []
         period = 2
         find_pivot_lows(low, period)
 
 
-def test_find_pivot_lows_raises_type_error_for_invalid_period():
+def test_find_pivot_lows_type_error_invalid_period():
     with pytest.raises(TypeError):
-        low = pd.Series([10, 12, 14])
+        low = pd.Series()
         period = "2"
         find_pivot_lows(low, period)
 
 
-def test_find_pivot_lows_raises_value_error_for_invalid_period():
+def test_find_pivot_lows_value_error_invalid_period():
     with pytest.raises(ValueError):
-        low = pd.Series([10, 12, 14])
+        low = pd.Series()
         period = 0
         find_pivot_lows(low, period)
 
 
-def test_find_pivot_lows_returns_correct_structure():
-    low = pd.Series([10, 12, 14])
+def test_find_pivot_lows_value_returns_correct_structure():
+    low = pd.Series()
     period = 2
     result = find_pivot_lows(low, period)
     assert isinstance(result, pd.Series)
 
 
-def test_find_pivot_lows_returns_correct_values():
-    low = pd.Series([10.0, 8.0, 4.0, 5.0, 4.0, 3.0, 9.0, 11.0])
+def test_find_pivot_lows_value_returns_correct_values():
+    low = pd.Series(
+        [100, 90, 80, 90, 100],
+        index=[
+            "2025-10-15 00:00:00-04:00",
+            "2025-10-16 00:00:00-04:00",
+            "2025-10-17 00:00:00-04:00",
+            "2025-10-20 00:00:00-04:00",
+            "2025-10-21 00:00:00-04:00",
+        ],
+    )
     period = 2
     result = find_pivot_lows(low, period)
-    expected = pd.Series([4.0, 3.0], index=[2, 5])
+    expected = pd.Series([80.0], index=["2025-10-17 00:00:00-04:00"])
     assert pd.Series.equals(result, expected)
 
 
-def test_find_pivot_highs_raises_type_error_for_invalid_high():
+###
+# FIND THE LOW PRICE ZONES
+###
+
+
+def test_find_the_low_price_zones_type_error_invalid_low_pivots():
     with pytest.raises(TypeError):
-        high = []
+        low_pivots = []
+        atr = pd.Series()
+        find_the_low_price_zones(low_pivots, atr)
+
+
+def test_find_the_low_price_zones_type_error_invalid_atr():
+    with pytest.raises(TypeError):
+        low_pivots = pd.Series()
+        atr = []
+        find_the_low_price_zones(low_pivots, atr)
+
+
+def test_find_the_low_price_zones_returns_correct_structure():
+    low_pivots = pd.Series(
+        [200, 190],
+        index=[
+            "2025-11-11 00:00:00-05:00",
+            "2025-11-21 00:00:00-05:00",
+        ],
+    )
+    atr = pd.Series(
+        [5, 4],
+        index=[
+            "2025-11-11 00:00:00-05:00",
+            "2025-11-21 00:00:00-05:00",
+        ],
+    )
+    result = find_the_low_price_zones(low_pivots, atr)
+    expected = pd.DataFrame(
+        {
+            "date": ["2025-11-11 00:00:00-05:00", "2025-11-21 00:00:00-05:00"],
+            "type": ["low", "low"],
+            "bottom": [200, 190],
+            "top": [201.5, 191.2],
+        }
+    )
+    assert pd.DataFrame.equals(result, expected)
+
+
+###
+# FIND PIVOTS HIGHS
+###
+
+
+def test_find_pivot_high_type_error_invalid_low():
+    with pytest.raises(TypeError):
+        low = []
         period = 2
-        find_pivot_highs(high, period)
+        find_pivot_high(low, period)
 
 
-def test_find_pivot_highs_raises_type_error_for_invalid_period():
+def test_find_pivot_high_type_error_invalid_period():
     with pytest.raises(TypeError):
-        high = pd.Series([100, 102, 103])
+        low = pd.Series()
         period = "2"
-        find_pivot_highs(high, period)
+        find_pivot_high(low, period)
 
 
-def test_find_pivot_highs_raises_value_error_for_invalid_period():
+def test_find_pivot_high_value_error_invalid_period():
     with pytest.raises(ValueError):
-        high = pd.Series([100, 102, 103])
+        low = pd.Series()
         period = 0
-        find_pivot_highs(high, period)
+        find_pivot_high(low, period)
 
 
-def test_find_pivot_highs_returns_correct_structure():
-    high = pd.Series([100, 102, 103])
+def test_find_pivot_high_value_returns_correct_structure():
+    low = pd.Series()
     period = 2
-    result = find_pivot_highs(high, period)
+    result = find_pivot_high(low, period)
     assert isinstance(result, pd.Series)
 
 
-def test_find_pivot_highs_returns_correct_values():
-    high = pd.Series([40, 30, 50, 30, 40, 100, 30, 40])
+def test_find_pivot_high_value_returns_correct_values():
+    low = pd.Series(
+        [80, 90, 100, 90, 80],
+        index=[
+            "2025-10-15 00:00:00-04:00",
+            "2025-10-16 00:00:00-04:00",
+            "2025-10-17 00:00:00-04:00",
+            "2025-10-20 00:00:00-04:00",
+            "2025-10-21 00:00:00-04:00",
+        ],
+    )
     period = 2
-    result = find_pivot_highs(high, period)
-    expected = pd.Series([50, 100], index=[2, 5])
+    result = find_pivot_high(low, period)
+    expected = pd.Series([100.0], index=["2025-10-17 00:00:00-04:00"])
     assert pd.Series.equals(result, expected)
+
+
+###
+# FIND THE LOW PRICE ZONES
+###
+
+
+def test_find_the_high_price_zones_type_error_invalid_low_pivots():
+    with pytest.raises(TypeError):
+        low_pivots = []
+        atr = pd.Series()
+        find_the_high_price_zones(low_pivots, atr)
+
+
+def test_find_the_high_price_zones_type_error_invalid_atr():
+    with pytest.raises(TypeError):
+        low_pivots = pd.Series()
+        atr = []
+        find_the_high_price_zones(low_pivots, atr)
+
+
+def test_find_the_high_price_zones_returns_correct_structure():
+    low_pivots = pd.Series(
+        [200, 190],
+        index=[
+            "2025-11-11 00:00:00-05:00",
+            "2025-11-21 00:00:00-05:00",
+        ],
+    )
+    atr = pd.Series(
+        [5, 4],
+        index=[
+            "2025-11-11 00:00:00-05:00",
+            "2025-11-21 00:00:00-05:00",
+        ],
+    )
+    result = find_the_high_price_zones(low_pivots, atr)
+    expected = pd.DataFrame(
+        {
+            "date": ["2025-11-11 00:00:00-05:00", "2025-11-21 00:00:00-05:00"],
+            "type": ["high", "high"],
+            "top": [200, 190],
+            "bottom": [
+                198.5,
+                188.8,
+            ],
+        }
+    )
+    assert pd.DataFrame.equals(result, expected)
